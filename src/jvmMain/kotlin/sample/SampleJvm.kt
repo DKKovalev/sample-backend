b5c45385d61d754d2e910cb51396cb7ec5ab9093
dkkovalev <ordo-hereticus@yandex.ru>
2019-10-25 10:55:40 +0300

    Initial commit

---
+++ src/jvmMain/kotlin/sample/SampleJvm.kt
@@ -0,0 +1,31 @@
+package sample
+
+import io.ktor.application.*
+import io.ktor.html.*
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.content.*
+import io.ktor.response.respond
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import kotlinx.html.*
+import java.io.*
+
+actual class Sample {
+    actual fun checkMe() = 42
+}
+
+actual object Platform {
+    actual val name: String = "JVM"
+}
+
+fun main() {
+    val port = System.getenv("PORT")?.toInt() ?: 8080
+    embeddedServer(Netty, port = port) {
+        routing {
+            get("hello") {
+                call.respond(HttpStatusCode.Accepted, "Well hello there")
+            }
+        }
+    }.start(wait = true)
+}
